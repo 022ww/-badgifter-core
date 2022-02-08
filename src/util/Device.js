a70d79291a0af879d3b9469f1339e82101b852ba
Bad Gifter <50094885+badgifter@users.noreply.github.com>
2022-02-08 06:57:07 +0300

    remove options

--- src/util/Device.js
+++ src/util/Device.js
@@ -1,20 +1,20 @@
 
-let device;
+let device = 'nodejs_env';
 
-if(typeof navigator === 'undefined'){
-	device = 'nodejs_env'
-} else {
-	const {DeviceUUID} = require('device-uuid');
+// if(typeof navigator === 'undefined'){
+// 	device = 'nodejs_env'
+// } else {
+// 	const {DeviceUUID} = require('device-uuid');
 
-	const du = DeviceUUID().parse();
-	const dua = [
-		du.language,
-		du.platform,
-		du.os,
-		du.cpuCores,
-		du.colorDepth,
-	];
-	device = du.hashMD5(dua.join(':'));
-}
+// 	const du = new DeviceUUID().parse();
+// 	const dua = [
+// 		du.language,
+// 		du.platform,
+// 		du.os,
+// 		du.cpuCores,
+// 		du.colorDepth,
+// 	];
+// 	device = du.hashMD5(dua.join(':'));
+// }
 
 export default device;
