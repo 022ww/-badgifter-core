c6648f6a9b3742bdf387dc4d56453641da0ec377
Bad Gifter <badgifter@gmail.com>
2022-12-01 18:46:36 +0300

    fix?

--- src/util/Device.js
+++ src/util/Device.js
@@ -6,15 +6,15 @@ if(typeof navigator === 'undefined'){
 	device = 'nodejs_env'
 } else {
 
-// 	const du = new DeviceUUID().parse();
-// 	const dua = [
-// 		du.language,
-// 		du.platform,
-// 		du.os,
-// 		du.cpuCores,
-// 		du.colorDepth,
-// 	];
-// 	device = du.hashMD5(dua.join(':'));
-// }
+	const du = new DeviceUUID().parse();
+	const dua = [
+		du.language,
+		du.platform,
+		du.os,
+		du.cpuCores,
+		du.colorDepth,
+	];
+	device = du.hashMD5(dua.join(':'));
+}
 
 export default device;
